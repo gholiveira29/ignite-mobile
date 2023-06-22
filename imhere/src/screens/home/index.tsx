2655b23e0d81e5d51b250ec5460750c86cee5e77
gholiveira29 <g.h.p.o3615@gmail.com>
2023-06-21 21:56:20 -0300

    crinado form da home

--- imhere/src/screens/home/index.tsx
+++ imhere/src/screens/home/index.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
-export default function Home() {
+export function Home() {
+    function handleParticipantAdd() {
+        console.log('você clicou no adicionar');
+    }
     return (
         <View style={styles.container}>
             <Text style={styles.eventName}>Nome do Evento</Text>
             <Text style={styles.eventDate}>Sexta, 4 de junho de 2023</Text>
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Nome do participante'
+                    placeholderTextColor={'#6b6b6b'}
+                />
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
--- imhere/src/screens/home/styles.ts
+++ imhere/src/screens/home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
         padding: 24
     },
     eventName: {
-        color: '#fff',
+        color: '#FFF',
         fontSize: 24,
         fontWeight: 'bold',
         marginTop: 48,
@@ -15,5 +15,33 @@ export const styles = StyleSheet.create({
     eventDate: {
         color: '#6B6B6B',
         fontSize: 16
+    },
+    input: {
+        backgroundColor: '#1F1E25',
+        height: 56,
+        borderRadius: 10,
+        color: '#FFF',
+        padding: 16,
+        fontSize: 16,
+        flex: 1,
+        marginRight: 12
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 24
+    },
+    button: {
+        width: 56,
+        height: 56,
+        borderRadius: 10,
+        backgroundColor: '#31CF67',
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    form: {
+        width: '100%',
+        flexDirection: 'row',
+        marginTop: 36,
+        marginBottom: 42
     }
 });
